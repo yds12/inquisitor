e8531659cf5ffae51bba2c8c017e1d1b66b295a0
yds12 <yds12@protonmail.com>
2022-11-03 14:55:29 +0100

    Header option. Closes #2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use clap::{AppSettings, Parser as _, ValueEnum};
 use hdrhistogram::Histogram;
 use reqwest::ClientBuilder;
+use std::collections::HashMap;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use tokio::sync::{Mutex, MutexGuard};
+use tokio::sync::Mutex;
 
 const ITERATIONS: usize = 1000;
 const MAX_CONNS: usize = 12;
@@ -42,10 +43,26 @@ pub struct Config {
     /// Body of the HTTP request (only used if method is POST)
     #[clap(long, short = 'b', value_parser)]
     request_body: Option<String>,
+    /// Header entry for the HTTP request.
+    ///
+    /// The value should be in a KEY:VALUE format. Multiple key-value pairs can
+    /// be passed, e.g.: `-h Content-Type:application/json -h SomeKey:SomeValue
+    #[clap(long, short = 'h', value_parser)]
+    header: Vec<String>,
+    /// Do not print errors
+    #[clap(long, action)]
+    hide_errors: bool,
 }
 
 fn main() {
     let config = Config::parse();
+
+    let mut headers = HashMap::new();
+    for header in config.header {
+        if let Some((k, v)) = header.split_once(':') {
+            headers.insert(k.to_string(), v.to_string());
+        }
+    }
 
     // histogram of response times, recorded in microseconds
     let times = Arc::new(Mutex::new(
@@ -81,6 +98,7 @@ fn main() {
         let passes = passes.clone();
         let errors = errors.clone();
         let url = config.url.clone();
+        let headers = headers.clone();
         let failed_regex = failed_regex.clone();
         let times = times.clone();
 
@@ -88,12 +106,21 @@ fn main() {
             let mut total = passes.load(Ordering::Relaxed) + errors.load(Ordering::Relaxed);
 
             while total < config.iterations {
-                let req_start_time = std::time::SystemTime::now();
-                let response = match (config.method, request_body.as_deref()) {
-                    (Method::GET, _) => client.get(&url).send().await,
-                    (Method::POST, Some(body)) => client.post(&url).body(body).send().await,
-                    (Method::POST, None) => client.post(&url).send().await,
+                let mut builder = match config.method {
+                    Method::GET => client.get(&url),
+                    Method::POST => client.post(&url),
                 };
+
+                if let Some(body) = request_body.as_deref() {
+                    builder = builder.body(body);
+                }
+
+                for (k, v) in &headers {
+                    builder = builder.header(k, v);
+                }
+
+                let req_start_time = std::time::SystemTime::now();
+                let response = builder.send().await;
                 let elapsed = req_start_time.elapsed().unwrap().as_micros() as u64;
                 times
                     .lock()
@@ -115,22 +142,30 @@ fn main() {
                         let body = res.text().await.unwrap();
 
                         if failed_regex.as_ref().unwrap().is_match(&body) {
-                            eprintln!("Response is 200 but body indicates an error: {}", body);
+                            if !config.hide_errors {
+                                eprintln!("Response is 200 but body indicates an error: {}", body);
+                            }
                             errors.fetch_add(1, Ordering::SeqCst);
                         } else {
                             passes.fetch_add(1, Ordering::SeqCst);
 
                             if config.print_response {
-                                eprintln!("Response successful. Contents: {}", body);
+                                if !config.hide_errors {
+                                    eprintln!("Response successful. Contents: {}", body);
+                                }
                             }
                         }
                     }
                     Ok(res) if !res.status().is_success() => {
-                        eprintln!("Response is not 200. Status code: {}", res.status());
+                        if !config.hide_errors {
+                            eprintln!("Response is not 200. Status code: {}", res.status());
+                        }
                         errors.fetch_add(1, Ordering::SeqCst);
                     }
                     Err(e) => {
-                        eprintln!("Request failed: {}", e);
+                        if !config.hide_errors {
+                            eprintln!("Request failed: {}", e);
+                        }
                         errors.fetch_add(1, Ordering::SeqCst);
                     }
                     _ => unreachable!(),
@@ -145,16 +180,28 @@ fn main() {
 
     let times = rt.block_on(async {
         futures::future::join_all(handles).await;
-        Arc::try_unwrap(times).expect("bug: could not unwrap Arc").into_inner()
+        Arc::try_unwrap(times)
+            .expect("bug: could not unwrap Arc")
+            .into_inner()
     });
 
     let elapsed_ms = test_start_time.elapsed().unwrap().as_millis() as f64;
-    print_results(config.iterations, times, elapsed_ms,
-    errors.load(Ordering::Relaxed), passes.load(Ordering::Relaxed));
+    print_results(
+        config.iterations,
+        times,
+        elapsed_ms,
+        errors.load(Ordering::Relaxed),
+        passes.load(Ordering::Relaxed),
+    );
 }
 
-fn print_results(iterations: usize, times: Histogram<u64>, elapsed_ms: f64,
-                 errors: usize, passes: usize) {
+fn print_results(
+    iterations: usize,
+    times: Histogram<u64>,
+    elapsed_ms: f64,
+    errors: usize,
+    passes: usize,
+) {
     let rps = (iterations as f64 / (elapsed_ms / 1_000.0)) as usize;
 
     println!(
